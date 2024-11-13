3c992f8954411faf6d748c5d46f6f979ffe99be1
Pifo01 <mati.diaz121@gmail.com>
2024-11-13 14:21:20 -0300

    cambios a readme, creacion de requeriments y ajustes en settings

--- SistemaCMAS/settings.py
+++ SistemaCMAS/settings.py
@@ -51,10 +51,14 @@ WSGI_APPLICATION = 'SistemaCMAS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gimnasio',
+        'USER': 'root',
     }
 }
 
